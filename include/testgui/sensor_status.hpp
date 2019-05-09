33f681af0db5f1b942f6bad71610758ebdacca6b
zhu.zhuo <zhu.zhuo@horizon.ai>
2019-05-09 16:10:03 +0800

    edit sensor ip

--- include/testgui/sensor_status.hpp
+++ include/testgui/sensor_status.hpp
@@ -3,16 +3,16 @@
 #include <QThread>
 
 #define net_G4_NET    "www.baidu.com"
-#define net_IBEO_ECU    "192.168.1.201"
-#define net_VLP16FL     "192.168.1.201"
-#define net_VLP16FR     "192.168.1.201"
-#define net_VLP16R      "192.168.1.201"
-#define net_VLP16L      "192.168.1.201"
-#define net_VLP16REAR   "192.168.1.201"
-#define net_SICK        "192.168.1.201"
+#define net_IBEO_ECU    "192.168.0.100"
+#define net_VLP16FL     "192.168.0.201"
+#define net_VLP16FR     "192.168.0.205"
+#define net_VLP16R      "192.168.0.204"
+#define net_VLP16L      "192.168.0.202"
+#define net_VLP16REAR   "192.168.0.203"
+#define net_SICK        "192.168.1.41"
 #define net_PANDAR      "192.168.1.201"
-#define net_CAN         "192.168.1.201"
-#define net_RTK         "192.168.1.201"
+#define net_CAN         "192.168.1.111"
+#define net_RTK         "192.168.1.222"
 //#define SENSER_MAX  20
 
 
